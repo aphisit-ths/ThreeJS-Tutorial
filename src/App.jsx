35564c920243e0bbb3ac055858f66924ab33e4ac
Oath <82308226+apisit-kmitl@users.noreply.github.com>
2022-03-26 00:51:44 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <>
+        <div>Hello world</div>
+      </>
+    </div>
+  );
+}
+
+export default App;
